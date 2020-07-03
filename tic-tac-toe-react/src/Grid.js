4735d184b409ddfafd5c0f6f0a8507e58883f7fa
Oliver Kocsis <oliver.kocsis@gmail.com>
2020-07-03 19:46:36 +0200

    Grid template

---
+++ tic-tac-toe-react/src/Grid.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Grid.css';
+import Space from './Space'
+
+function Grid() {
+  return (
+    <div className="Grid">
+      <Space />
+      <Space />
+      <Space />
+      <Space />
+      <Space />
+      <Space />
+      <Space />
+      <Space />
+      <Space />
+    </div>
+  );
+}
+
+export default Grid;
---
+++ tic-tac-toe-react/src/Space.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Space.css'
+
+function Space() { 
+
+  const mark = function () {
+
+  };
+
+  return (<div className="Space">X</div>);
+}
+
+export default Space;
